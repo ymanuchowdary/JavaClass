3ce3e6b007a0ccadd73ac07674237dd31b2cdc77
Manu Chowdary <ymanoj1@hotmail.com>
2018-03-20 15:56:48 -0500

    Interface

---
+++ InterfaceWrestler.java
@@ -0,0 +1,8 @@
+package com.practice.javaclass;
+
+public interface InterfaceWrestler {
+
+	public abstract void themeMusic();
+	public abstract void finisher();
+	public abstract void paymentForWork(int hours);
+}
